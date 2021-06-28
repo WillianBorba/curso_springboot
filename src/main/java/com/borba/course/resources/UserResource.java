5168d597be092a717ffb22dd47acf6977e015b1a
WillianBorba <willian.rocha.borba@gmail.com>
2021-06-28 20:46:04 -0300

    User entity and resource

---
+++ src/main/java/com/borba/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.borba.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.borba.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "UserTeste", "emailTeste@gmail.com", "1234567859", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
