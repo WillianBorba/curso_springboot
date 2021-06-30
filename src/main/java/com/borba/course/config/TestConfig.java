96e7d4d45155808db592d33c5cd0190f534169de
WillianBorba <willian.rocha.borba@gmail.com>
2021-06-29 21:35:52 -0300

    Adicionado enum OrderStatus, Adicionado campo OrderStatus em Order

--- src/main/java/com/borba/course/config/TestConfig.java
+++ src/main/java/com/borba/course/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.borba.course.entities.Order;
 import com.borba.course.entities.User;
+import com.borba.course.entities.enums.OrderStatus;
 import com.borba.course.repositories.OrderRepository;
 import com.borba.course.repositories.UserRepository;
 
@@ -30,10 +31,10 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o = new Order(null, Instant.parse("2020-06-19T19:53:07Z"), u); 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o = new Order(null, Instant.parse("2020-06-19T19:53:07Z"),OrderStatus.PAID ,u); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.WAITING_PAYMENT ,u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT ,u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT ,u1);
 		
 		userRepository.saveAll(Arrays.asList(u, u1, u2));
 		orderRepositoy.saveAll(Arrays.asList(o , o1, o2, o3));
