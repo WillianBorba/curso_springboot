afa886a9de6cf3b4c191602445384b3ddf20468c
WillianBorba <willian.rocha.borba@gmail.com>
2021-07-03 14:29:11 -0300

    Adicionado OrderItem e seus relacionamentos com Item e Order

--- src/main/java/com/borba/course/config/TestConfig.java
+++ src/main/java/com/borba/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.borba.course.entities.Category;
 import com.borba.course.entities.Order;
+import com.borba.course.entities.OrderItem;
 import com.borba.course.entities.Product;
 import com.borba.course.entities.User;
 import com.borba.course.entities.enums.OrderStatus;
 import com.borba.course.repositories.CategoryRepository;
+import com.borba.course.repositories.OrderItemRepository;
 import com.borba.course.repositories.OrderRepository;
 import com.borba.course.repositories.ProductRepository;
 import com.borba.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,5 +77,12 @@ public class TestConfig implements CommandLineRunner{
 
 		userRepository.saveAll(Arrays.asList(u, u1, u2));
 		orderRepositoy.saveAll(Arrays.asList(o , o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
 }
